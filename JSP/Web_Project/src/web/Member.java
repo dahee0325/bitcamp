c71a3822c6e77cee0a8e36ff535adec819dcd2a7
dahee <dahee03250@naver.com>
2019-06-21 18:28:56 +0900

    0621 JSP

---
+++ JSP/Web_Project/src/web/Member.java
@@ -0,0 +1,49 @@
+package web;
+
+public class Member {
+
+	private	String name;
+	private String id;
+	private String pNum;
+	
+	//public : 다른곳에서 인스턴스생성시 문제가 생기지않도록
+	public Member(String name, String id, String pNum) {
+		
+		this.name = name;
+		this.id = id;
+		this.pNum = pNum;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getpNum() {
+		return pNum;
+	}
+
+	public void setpNum(String pNum) {
+		this.pNum = pNum;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Member [name=" + name + ", id=" + id +", pNum =" + pNum + "]";
+	}
+
+	
+}
