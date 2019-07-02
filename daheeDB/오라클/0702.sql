393307e89f01f8dfdead24a243761fe10f8a69ad
dahee <dahee03250@naver.com>
2019-07-02 17:11:52 +0900

    naver del

--- daheeDB/오라클/0702.sql
+++ daheeDB/오라클/0702.sql
@@ -27,3 +27,27 @@ select ename, sal, sal + 100 from emp;
 select ename, sal, sal - 100 from emp;
 select ename, sal, sal * 12 from emp;
 select ename, sal, sal / 5 from emp;
+
+--nvl 함수 : 특정 컬럼의 데이터가 null인경우 특정 데이터로 치환해주는 함수
+--nvl(컬럼이름, 치환값)
+select ename, sal, comm, sal*12+comm from emp;
+select ename, sal, comm, sal*12+nvl(comm,0) from emp;
+select ename, sal, comm, nvl(comm,0), sal*12+nvl(comm,0) from emp;
+--컬럼의 이름 지어주기 as 또는 띄어쓰기 후 이름지정
+select ename, sal, comm, nvl(comm,0), sal*12+nvl(comm,0) as ySall from emp;
+select ename, sal, comm, nvl(comm,0), sal*12+nvl(comm,0) ySall from emp;
+
+--컬럼과 문자열을 연결하는 결과 출력, sql 에서 문자열 표현은 '' 로 묶는다.
+select ename || ' is a ' || job str from emp;
+
+--distinct : 컬럼값에서 중복되는값을 제거하고 하나의 데이터만 출력해준다.
+select deptno from emp;
+select distinct deptno from emp;
+--두개의 컬럼값을 distinct 로 하면 두개의 값이 중복되지않게 출력해준다.ㅊㅇ
+select distinct deptno, job from emp;
+
+--select 의 조건절 : select 컬럼명 from 테이블명 where 조건식(true/false);
+--where 의 조건은 행(row)을 찾는다 : 조건에 맞는 행을 출력
+--deptno가 10인 dname을 출력
+select dname from dept where deptno = 10;
+select empno, ename,job from emp where deptno = 10;
