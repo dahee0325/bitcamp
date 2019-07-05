2d1d60dbca39d297003d6c489dd02a08bacd441c
dahee <dahee03250@naver.com>
2019-07-05 18:20:57 +0900

    DB update

--- daheeDB/오라클/0703_과제.sql
+++ daheeDB/오라클/0703_과제.sql
@@ -211,6 +211,7 @@ from emp
 select job "직급", min(sal) "최저 급여"
 from emp
 where mgr>0
+--where mgr is not null
 group by job
 having min(sal) > 2000
 order by min(sal) desc
@@ -235,4 +236,61 @@ select job "JOB",deptno "DNO",decode(deptno,10,sum(sal)) "부서10", decode(dept
 from emp
 group by deptno, job
 order by deptno
+;
+
+
+----------------------------------------- 과제4 ------------------------------------------------------
+--32. EQUI 조인을 사용하여 SCOTT 사원의 부서번호와 부서 이름을 출력하시오.
+select d.deptno "부서번호", d.dname "부서이름"
+from emp e, dept d
+where e.deptno=d.deptno and e.ename='SCOTT'
+;
+
+--33. INNER JOIN과 ON 연산자를 사용하여 사원 이름과 함께 그 사원이 소속된 부서이름과 지역 명을 출력하시오.
+select e.ename "사원이름", d.dname "부서이름", d.loc "지역명"
+from emp e inner join dept d
+on e.deptno = d.deptno
+;
+
+--36. 조인과 WildCARD를 사용하여 이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
+select e.ename "사원이름", d.dname "부서이름"
+from emp e, dept d
+where e.deptno=d.deptno and e.ename like '%A%'
+;
+
+--37. JOIN을 이용하여 NEW YORK에 근무하는 모든 사원의 이름, 업무, 부서번호 및 부서명을 출력하시오.
+select e.ename "사원이름",e.job "업무", d.deptno "부서번호", d.dname "부서이름"
+from emp e, dept d
+where e.deptno=d.deptno and d.loc = 'NEW YORK'
+;
+
+--38. SELF JOIN을 사용하여 사원의 이름 및 사원번호, 관리자 이름을 출력하시오.
+select m.ename "사원이름", m.empno "사원번호", e.ename "관리자 이름"
+from emp e, emp m
+where e.empno = m.mgr
+;
+
+--39. OUTER JOIN, SELF JOIN을 사용하여 관리자가 없는 사원을 포함하여 사원번호를 기준으로 내림차순 정렬하여 출력하시오.
+select m.ename "사원이름", m.empno "사원번호", e.ename "관리자 이름"
+from emp e, emp m
+where e.empno(+) = m.mgr
+order by e.empno desc
+;
+
+--40. SELF JOIN을 사용하여 지정한 사원의 이름, 부서번호, 지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. ( SCOTT )
+select e.ename "지정한사원이름", e.deptno "부서번호", m.ename "사원이름"
+from emp e, emp m
+where e.deptno = m.deptno and e.ename = 'SCOTT'
+;
+
+--41. SELF JOIN을 사용하여 WARD 사원보다 늦게 입사한 사원의 이름과 입사일을 출력하시오.
+select m.ename "사원이름", m.hiredate "입사일"
+from emp e, emp m
+where e.ename = 'WARD' and e.hiredate < m.hiredate
+;
+
+--42. SELF JOIN 을 사용하여 관리자보다 먼저 입사한 모든 사원의 이름 및 입사일을 관리자의 이름 및 입사일과 함께 출력하시오.
+select m.ename "사원이름" , m.hiredate "사원 입사일", e.ename "관리자이름", e.hiredate "관리자 입사일"
+from emp e, emp m
+where e.empno = m.mgr and e.hiredate > m.hiredate
 ;
